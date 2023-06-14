0450e997f316d38d4210a4fe135ddca183543441
Janay Ma <janay.ma7@gmail.com>
2023-06-14 16:46:44 -0700

    refactor breedFetcher to use more modular code. Put user facing output in index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,29 @@
 const request = require('request');
 
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("No information found for that breed. Maybe it does not exist");
-    return;
-  }
-  //console.log(data, typeof(data));
 
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    let desc = null;
+
+    //if search returns nothing
+    if (data.length === 0) {
+      error = "No information found for that breed. Maybe it does not exist"
+    } else {
+      //fetch description from data
+      desc = data[0].description;
+    }
+
+    callback(error, desc);
+  });
+}
+
+module.exports = { fetchBreedDescription };
+
+
+
+
+
